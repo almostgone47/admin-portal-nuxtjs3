d212f6c6864a460353c8b5d1ed787d6410a41b42
almostgone47 <almostgone47@hotmail.com>
2022-09-26 23:54:34 +0800

    add express server and passport

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+import { Strategy as JwtStrategy } from "passport-jwt";
+import extractors from "passport-jwt/lib/extract_jwt.js";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const opts = {};
+opts.jwtFromRequest = extractors.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.secretOrKey;
+
+const passportStrategy = (passport) => {
+  // need to send bearer token in auth headers
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      console.log("PASSPORT:: ", jwt_payload);
+      try {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: jwt_payload.email,
+          },
+        });
+
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })
+  );
+};
+
+export default passportStrategy;
---
+++ server/middleware/index.ts
@@ -0,0 +1,114 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+// Passport is Express-compatible authentication middleware for Node.js.
+import passportConfig from "../../config/passport";
+
+const prisma = new PrismaClient();
+const app = express();
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+passportConfig(passport);
+
+app.use(express.json());
+
+app.get(`/test`, (req, res) => {
+  res.send("the api works!!");
+});
+
+app.post(`/api/register`, async (req, res) => {
+  // if user already exists return error msg
+  const user = await prisma.user.findFirst({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (user) {
+    return res.status(400).json({ msg: "Invalid email. User already exists." });
+  }
+  // else create new user with hashed password
+  const newUser = {
+    email: req.body.email.trim().toLowerCase(),
+    password: req.body.password, // This will be a hash
+  };
+  // Generate hash for password
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, async (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      // create new user
+      const payload = await prisma.user.create({
+        data: { email: newUser.email, password: newUser.password },
+      });
+
+      // return signed jwt tokent to client side
+      jwt.sign(
+        payload,
+        process.env.secretOrKey,
+        { expiresIn: 3600 },
+        (err, token) => {
+          return res.status(200).json({
+            jwtToken: "Bearer " + token,
+            user: payload,
+          });
+        }
+      );
+    });
+  });
+});
+
+app.post(`/api/login`, async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+    select: {
+      email: true,
+      password: true,
+    },
+  });
+
+  if (user) {
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (user && isMatch) {
+      // User matched, create JWT Payload
+      const payload = {
+        email: user.email,
+      };
+      // Sign token (expires in 1 hour or 3600 seconds)
+      jwt.sign(
+        payload,
+        process.env.secretOrKey,
+        { expiresIn: 3600 },
+        (err, token) => {
+          return res.status(200).json({
+            success: true,
+            jwtToken: "Bearer " + token,
+            user: payload,
+          });
+        }
+      );
+    } else {
+      return res.status(400).json({ msg: "Incorrect login information" });
+    }
+  }
+});
+
+app.get(
+  "/api/current-user",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json("some shit");
+  }
+);
+
+export default {
+  path: "/api",
+  handler: app,
+};
